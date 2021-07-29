63c826e6384c1c9506c293f5fe7645608cc54353
joaofelipebarrosneto <jfbnfelipe.jfbn@gmail.com>
2021-07-29 01:23:37 -0300

    push project into github

---
+++ src/test/java/com/parimpar/parimparjob/ParImparJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parimpar.parimparjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParImparJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
